0bfbab93ad64dd2ece258b21ae1dbbbece4649d5
Duncan Watson-Parris <duncan.watson-parris@physics.ox.ac.uk>
2020-04-01 09:46:29 +0100

    Fix command line options and patch the tf device list for keras compatibility

--- poc_model.py
+++ poc_model.py
@@ -1,6 +1,29 @@
+#!/usr/bin/env python
 import numpy as np
 import os
 import argparse
+
+# Patch the tf 2 available gpus fn
+import tensorflow as tf
+import keras.backend.tensorflow_backend as tfback
+print("tf.__version__ is", tf.__version__)
+print("tf.keras.__version__ is:", tf.keras.__version__)
+
+def _get_available_gpus():
+    """Get a list of available gpu devices (formatted as strings).
+
+    # Returns
+        A list of available GPU devices.
+    """
+    #global _LOCAL_DEVICES
+    if tfback._LOCAL_DEVICES is None:
+        devices = tf.config.list_logical_devices()
+        tfback._LOCAL_DEVICES = [x.name for x in devices]
+    return [x for x in tfback._LOCAL_DEVICES if 'device:gpu' in x.lower()]
+
+tfback._get_available_gpus = _get_available_gpus
+####
+
 from refined_model.code.model_def import get_model as get_refined_model
 from rough_model.code.model_def import get_model as get_rough_model
 
@@ -29,10 +52,10 @@ if __name__ == '__main__':
     parser.add_argument('weights', help="Path to pre-trained model weights")
     parser.add_argument('-o', '--output', help="Output numpy mask stack file name", default='poc_mask.npz')
     parser.add_argument('-b', '--batch_size', help="Batch size", default=1)
-    parser.add_argument('-n', '--n_gpus', help="Number of GPUs to use", defaut=1)
-    parser.add_argument('-e', '--n_epochs', help="Number of epochs to run", defaut=1)
-    parser.add_argument('-l', '--learning_rate', help="(Starting) Learning rate ", defaut=0.01)
-    parser.add_argument('-d', '--decay_rate', help="Decay rate", defaut=0.0)
+    parser.add_argument('-n', '--no_gpus', help="Number of GPUs to use", default=1, type=int)
+    parser.add_argument('-e', '--n_epochs', help="Number of epochs to run", default=1)
+    parser.add_argument('-l', '--learning_rate', help="(Starting) Learning rate ", default=0.01)
+    parser.add_argument('-d', '--decay_rate', help="Decay rate", default=0.0)
 
     # Gets command line args by default
     args = parser.parse_args()
@@ -42,14 +65,14 @@ if __name__ == '__main__':
 
     print('Input dataset shape = ', dataset.shape)
 
-    rough_mask_model = get_rough_model(args.weights + 'rough_model_weights.h5', args.learning_rate,
-                                       args.decay_rate, args.n_gpus)
+    rough_mask_model = get_rough_model(args.weights + 'rough_model_weights_new.h5', args.learning_rate,
+                                       args.decay_rate, args.no_gpus)
 
     refined_mask_model = get_refined_model(args.weights+'refine_model_weights.h5', args.learning_rate,
-                                           args.decay_rate, args.n_gpus)
+                                           args.decay_rate, args.no_gpus)
 
     # create the rough masks
-    mask = rough_mask_model.predict(dataset, batch_size=4*args.n_gpus)
+    mask = rough_mask_model.predict(dataset, batch_size=4*args.no_gpus)
 
     # How many rough POCs?
     print("Roughly detected POCs: {}".format(mask.any(axis=0).sum()))
@@ -59,7 +82,7 @@ if __name__ == '__main__':
     dataset_masked = get_masked_dataset(mask, dataset)
 
     # refine the masked dataset
-    refined_mask = refined_mask_model.predict(dataset_masked, batch_size=4*args.n_gpus)
+    refined_mask = refined_mask_model.predict(dataset_masked, batch_size=4*args.no_gpus)
 
     # Create the final mask
     pre = refined_mask >= 0.5
